289dfbf2d45df2ad5ba028a83b615af5c587da06
Spring MC <Heresy.MC@gmail.com>
2015-09-23 07:58:45 +0800

    new color definitions

---
+++ colors.go
@@ -0,0 +1,31 @@
+package colorize
+
+const (
+	ColorNone = iota
+	ColorRed
+	ColorGreen
+	ColorYellow
+	ColorBlue
+	ColorMagenta
+	ColorCyan
+	ColorWhite
+	ColorGray
+	ColorBlack Color = -1
+)
+
+var (
+	colors = map[string]Color{
+		"red":     ColorRed,
+		"green":   ColorGreen,
+		"yellow":  ColorYellow,
+		"blue":    ColorBlue,
+		"magenta": ColorMagenta,
+		"cyan":    ColorCyan,
+		"white":   ColorWhite,
+		"gray":    ColorGray,
+		"black":   ColorBlack,
+	}
+)
+
+// Color can be used for foreground and background
+type Color int
