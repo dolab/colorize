7511720fe71c68927f9f085fffe7034cf9a57290
Spring MC <Heresy.MC@gmail.com>
2015-09-23 08:26:22 +0800

    new command line formatting property

---
+++ property_test.go
@@ -0,0 +1,14 @@
+package colorize
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Property_String(t *testing.T) {
+	ppt := Property{true, true, true, true, true, true}
+	assertion := assert.New(t)
+
+	assertion.Equal("1;5;4;7;", ppt.String())
+}
